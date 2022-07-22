96149ed46f09b5b87f3b5b2264e33a562c7a40ad
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-07-22 09:33:18 -0400

    added feature to change question and answer once clicked

--- assets/chal4JS.js
+++ assets/chal4JS.js
@@ -4,6 +4,19 @@ var questionPrompt = document.getElementById("question");
 var answerPrompt = document.getElementById("answers");
 var startButton = document.getElementById("startButton");
 
+var questionBank = ["question 1", "question 2", "question 3"];
+var questionIndex = 1;
+var completeAnswerBank = [
+    ["answer 1", "answer2", "answer 3", "AB4"],
+    ["question2answer 1", "answer2", "answer 3", "AB4"],
+    ["question3answer 1", "answer2", "answer 3", "AB4"],
+    ["question4answer 1", "answer2", "answer 3", "AB4"],
+];
+var completeAnswerIndex = 0;
+// var answerBank1 = ["answer 1", "answer2", "answer 3", "AB4"];
+// var answerBank2 = ["answer 1", "answer2", "answer 3", "AB4"];
+// var answerBank3 = ["answer 1", "answer2", "answer 3", "AB4"];
+
 startButton.addEventListener("click" , function(event){
 
     startButton.setAttribute("style", "display: none");
@@ -11,6 +24,15 @@ startButton.addEventListener("click" , function(event){
 
 });
 
+answerPrompt.addEventListener("click", function(){
+    questionPrompt.textContent = questionBank[questionIndex];
+    questionIndex++;
 
-answerPrompt.children[0].textContent = "test"
+    for(var i = 0; i < 4; i++){
+        answerPrompt.children[i].textContent = completeAnswerBank[completeAnswerIndex][i];
+    }
+    completeAnswerIndex++;
+
+});
+
 
